e0257ae1ffb26d0111041f49fda7e7a37a9801f5
Bella Gardner <isabella.v.gardner@gmail.com>
2024-01-11 15:55:19 +0100

    import update

--- dig/fairgraph/method/run.py
+++ dig/fairgraph/method/run.py
@@ -1,6 +1,8 @@
-from .Graphair import graphair,aug_module,GCN,GCN_Body,Classifier
-
+from .Graphair import graphair, aug_module, GCN, GCN_Body, Classifier
+from dig.fairgraph.dataset import POKEC, NBA
+import torch
 import time
+
 
 class run():
     r"""
@@ -10,8 +12,8 @@ class run():
     def __init__(self):
         pass
 
-    def run(self,device,dataset,model='Graphair',epochs=10_000,test_epochs=1_000,
-            lr=1e-4,weight_decay=1e-5):
+    def run(self, device, dataset, model='Graphair', epochs=10_000, test_epochs=1_000,
+            lr=1e-4, weight_decay=1e-5):
         r""" This method runs training and evaluation for a fairgraph model on the given dataset.
         Check :obj:`examples.fairgraph.Graphair.run_graphair_nba.py` for examples on how to run the Graphair model.
 
@@ -51,21 +53,33 @@ class run():
         idx_sens = dataset.idx_sens_train
 
         # generate model
-        if model=='Graphair':
+        if model == 'Graphair':
             aug_model = aug_module(features, n_hidden=64, temperature=1).to(device)
-            f_encoder = GCN_Body(in_feats = features.shape[1], n_hidden = 64, out_feats = 64, dropout = 0.1, nlayer = 2).to(device)
-            sens_model = GCN(in_feats = features.shape[1], n_hidden = 64, out_feats = 64, nclass = 1).to(device)
-            classifier_model = Classifier(input_dim=64,hidden_dim=64)
-            model = graphair(aug_model=aug_model,f_encoder=f_encoder,sens_model=sens_model,classifier_model=classifier_model, lr=lr,weight_decay=weight_decay,dataset=dataset_name).to(device)
+            f_encoder = GCN_Body(in_feats=features.shape[1], n_hidden=64, out_feats=64, dropout=0.1, nlayer=2).to(
+                device)
+            sens_model = GCN(in_feats=features.shape[1], n_hidden=64, out_feats=64, nclass=1).to(device)
+            classifier_model = Classifier(input_dim=64, hidden_dim=64)
+            model = graphair(aug_model=aug_model, f_encoder=f_encoder, sens_model=sens_model,
+                             classifier_model=classifier_model, lr=lr, weight_decay=weight_decay,
+                             dataset=dataset_name).to(device)
         else:
             raise Exception('At this moment, only Graphair is supported!')
-        
+
         # call fit_whole
         st_time = time.time()
-        model.fit_whole(epochs=epochs,adj=adj, x=features,sens=sens,idx_sens = idx_sens,warmup=0, adv_epoches=1)
+        model.fit_whole(epochs=epochs, adj=adj, x=features, sens=sens, idx_sens=idx_sens, warmup=0, adv_epoches=1)
         print("Training time: ", time.time() - st_time)
 
-
         # Test script
-        model.test(adj=adj,features=features,labels=dataset.labels,epochs=test_epochs,idx_train=dataset.idx_train,idx_val=dataset.idx_val,idx_test=dataset.idx_test,sens=sens)
+        model.test(adj=adj, features=features, labels=dataset.labels, epochs=test_epochs, idx_train=dataset.idx_train,
+                   idx_val=dataset.idx_val, idx_test=dataset.idx_test, sens=sens)
 
+
+# Load the dataset
+nba = NBA()
+
+# Train and evaluate
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+run_fairgraph = run()
+run_fairgraph.run(device, dataset=nba, model='Graphair', epochs=2_000, test_epochs=1_000,
+                  lr=1e-4, weight_decay=1e-5)
