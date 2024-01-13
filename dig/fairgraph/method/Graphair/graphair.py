e94b03f563786764252da215dd71b5893199e257
Isabella Gardner <75781502+bellavg@users.noreply.github.com>
2024-01-13 11:57:34 +0100

    changed to write instead of print. added save_path to init

--- dig/fairgraph/method/Graphair/graphair.py
+++ dig/fairgraph/method/Graphair/graphair.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import scipy.sparse as sp
 import numpy as np
-from dig.fairgraph.utils.utils import scipysp_to_pytorchsp,accuracy,fair_metric
+from dig.fairgraph.utils.utils import scipysp_to_pytorchsp, accuracy, fair_metric
+
 
 class graphair(nn.Module):
     r'''
@@ -49,8 +50,11 @@ class graphair(nn.Module):
         :type num_proj_hidden: int,optional
 
     '''
-    def __init__(self, aug_model, f_encoder, sens_model, classifier_model, lr = 1e-4, weight_decay = 1e-5, alpha = 20, beta = 1, gamma = 0.7, lam = 1, dataset = 'POKEC', num_hidden = 128, num_proj_hidden = 64):
+
+    def __init__(self, aug_model, f_encoder, sens_model, classifier_model, lr=1e-4, weight_decay=1e-5, alpha=20, beta=1,
+                 gamma=0.7, lam=1, dataset='POKEC', num_hidden=128, num_proj_hidden=64):
         super(graphair, self).__init__()
+        self.save_path = None
         self.aug_model = aug_model
         self.f_encoder = f_encoder
         self.sens_model = sens_model
@@ -62,30 +66,29 @@ class graphair(nn.Module):
         self.lam = lam
 
         self.criterion_sens = nn.BCEWithLogitsLoss()
-        self.criterion_cont= nn.CrossEntropyLoss()
+        self.criterion_cont = nn.CrossEntropyLoss()
         self.criterion_recons = nn.MSELoss()
 
-        self.optimizer_s = torch.optim.Adam(self.sens_model.parameters(), lr = 1e-4, weight_decay = 1e-5)
+        self.optimizer_s = torch.optim.Adam(self.sens_model.parameters(), lr=1e-4, weight_decay=1e-5)
 
-        FG_params = [{'params': self.aug_model.parameters(), 'lr': 1e-4} ,  {'params':self.f_encoder.parameters()}]
-        self.optimizer = torch.optim.Adam(FG_params, lr = lr, weight_decay = weight_decay)
+        FG_params = [{'params': self.aug_model.parameters(), 'lr': 1e-4}, {'params': self.f_encoder.parameters()}]
+        self.optimizer = torch.optim.Adam(FG_params, lr=lr, weight_decay=weight_decay)
 
-        self.optimizer_aug = torch.optim.Adam(self.aug_model.parameters(), lr = 1e-4, weight_decay = weight_decay)
-        self.optimizer_enc = torch.optim.Adam(self.f_encoder.parameters(), lr = lr, weight_decay = weight_decay)
-
+        self.optimizer_aug = torch.optim.Adam(self.aug_model.parameters(), lr=1e-4, weight_decay=weight_decay)
+        self.optimizer_enc = torch.optim.Adam(self.f_encoder.parameters(), lr=lr, weight_decay=weight_decay)
 
         self.fc1 = torch.nn.Linear(num_hidden, num_proj_hidden)
         self.fc2 = torch.nn.Linear(num_proj_hidden, num_hidden)
 
         self.optimizer_classifier = torch.optim.Adam(self.classifier.parameters(),
-                            lr=lr, weight_decay=weight_decay)
-    
+                                                     lr=lr, weight_decay=weight_decay)
+
     def projection(self, z):
         z = F.elu(self.fc1(z))
         return self.fc2(z)
-    
+
     def info_nce_loss_2views(self, features):
-        
+
         batch_size = int(features.shape[0] / 2)
 
         labels = torch.cat([torch.arange(batch_size) for i in range(2)], dim=0)
@@ -107,11 +110,10 @@ class graphair(nn.Module):
 
         logits = torch.cat([positives, negatives], dim=1)
         labels = torch.zeros(logits.shape[0], dtype=torch.long).cuda()
-        
+
         temperature = 0.07
         logits = logits / temperature
         return logits, labels
-
 
     def forward(self, adj, x):
         assert sp.issparse(adj)
@@ -122,93 +124,89 @@ class graphair(nn.Module):
         degree_mat_inv_sqrt = sp.diags(np.power(degrees, -0.5).flatten())
         adj_norm = degree_mat_inv_sqrt @ adj @ degree_mat_inv_sqrt
         adj_norm = scipysp_to_pytorchsp(adj_norm)
-    
-        adj = adj_norm.cuda()
-        return self.f_encoder(adj,x)
 
-    def fit_whole(self, epochs, adj, x,sens,idx_sens,warmup=None, adv_epoches=1):
+        adj = adj_norm.cuda()
+        return self.f_encoder(adj, x)
+
+    def fit_whole(self, epochs, adj, x, sens, idx_sens, warmup=None, adv_epoches=1):
         assert sp.issparse(adj)
         if not isinstance(adj, sp.coo_matrix):
             adj = sp.coo_matrix(adj)
         adj.setdiag(1)
         adj_orig = scipysp_to_pytorchsp(adj).to_dense()
-        norm_w = adj_orig.shape[0]**2 / float((adj_orig.shape[0]**2 - adj_orig.sum()) * 2)
+        norm_w = adj_orig.shape[0] ** 2 / float((adj_orig.shape[0] ** 2 - adj_orig.sum()) * 2)
         degrees = np.array(adj.sum(1))
         degree_mat_inv_sqrt = sp.diags(np.power(degrees, -0.5).flatten())
         adj_norm = degree_mat_inv_sqrt @ adj @ degree_mat_inv_sqrt
         adj_norm = scipysp_to_pytorchsp(adj_norm)
-        
 
         adj = adj_norm.cuda()
-        
+
         best_contras = float("inf")
-        
-        if warmup:
-            for _ in range(warmup):
-                adj_aug, x_aug, adj_logits = self.aug_model(adj, x, adj_orig = adj_orig.cuda())
+
+        # Open a file to write the results
+        with open('fit_whole_results.txt', 'w') as file:
+            if warmup:
+                for _ in range(warmup):
+                    adj_aug, x_aug, adj_logits = self.aug_model(adj, x, adj_orig=adj_orig.cuda())
+                    edge_loss = norm_w * F.binary_cross_entropy_with_logits(adj_logits, adj_orig.cuda())
+
+                    feat_loss = self.criterion_recons(x_aug, x)
+                    recons_loss = edge_loss + self.beta * feat_loss
+
+                    self.optimizer_aug.zero_grad()
+                    with torch.autograd.set_detect_anomaly(True):
+                        recons_loss.backward(retain_graph=True)
+                    self.optimizer_aug.step()
+
+                    file.write(
+                        f'edge reconstruction loss: {edge_loss.item():.4f} feature reconstruction loss: {feat_loss.item():.4f}\n')
+
+            for epoch_counter in range(epochs):
+                ### generate fair view
+                adj_aug, x_aug, adj_logits = self.aug_model(adj, x, adj_orig=adj_orig.cuda())
+
+                ### extract node representations
+                h = self.projection(self.f_encoder(adj, x))
+                h_prime = self.projection(self.f_encoder(adj_aug, x_aug))
+                # print("encoder done")
+
+                ## update sens model
+                adj_aug_nograd = adj_aug.detach()
+                x_aug_nograd = x_aug.detach()
+                if (epoch_counter == 0):
+                    sens_epoches = adv_epoches * 10
+                else:
+                    sens_epoches = adv_epoches
+                for _ in range(sens_epoches):
+                    s_pred, _ = self.sens_model(adj_aug_nograd, x_aug_nograd)
+                    senloss = self.criterion_sens(s_pred[idx_sens], sens[idx_sens].unsqueeze(1).float())
+                    self.optimizer_s.zero_grad()
+                    senloss.backward()
+                    self.optimizer_s.step()
+                s_pred, _ = self.sens_model(adj_aug, x_aug)
+                senloss = self.criterion_sens(s_pred[idx_sens], sens[idx_sens].unsqueeze(1).float())
+
+                ## update aug model
+                logits, labels = self.info_nce_loss_2views(torch.cat((h, h_prime), dim=0))
+                contrastive_loss = self.criterion_cont(logits, labels)
+
+                ## update encoder
                 edge_loss = norm_w * F.binary_cross_entropy_with_logits(adj_logits, adj_orig.cuda())
 
-                feat_loss =  self.criterion_recons(x_aug, x)
-                recons_loss =  edge_loss + self.beta * feat_loss
+                feat_loss = self.criterion_recons(x_aug, x)
+                recons_loss = edge_loss + self.lam * feat_loss
+                loss = self.beta * contrastive_loss + self.gamma * recons_loss - self.alpha * senloss
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
-                self.optimizer_aug.zero_grad()
-                with torch.autograd.set_detect_anomaly(True):
-                    recons_loss.backward(retain_graph=True)
-                self.optimizer_aug.step()
-
-                print(
-                'edge reconstruction loss: {:.4f}'.format(edge_loss.item()),
-                'feature reconstruction loss: {:.4f}'.format(feat_loss.item()),
-                )
-
-        for epoch_counter in range(epochs):
-            ### generate fair view
-            adj_aug, x_aug, adj_logits = self.aug_model(adj, x, adj_orig = adj_orig.cuda())
-            
-            ### extract node representations
-            h = self.projection(self.f_encoder(adj, x))
-            h_prime = self.projection(self.f_encoder(adj_aug, x_aug))
-            # print("encoder done")
-
-            ## update sens model
-            adj_aug_nograd = adj_aug.detach()
-            x_aug_nograd = x_aug.detach()
-            if (epoch_counter == 0):
-                sens_epoches = adv_epoches * 10
-            else:
-                sens_epoches = adv_epoches
-            for _ in range(sens_epoches):
-                s_pred , _  = self.sens_model(adj_aug_nograd, x_aug_nograd)
-                senloss = self.criterion_sens(s_pred[idx_sens],sens[idx_sens].unsqueeze(1).float())
-                self.optimizer_s.zero_grad()
-                senloss.backward()
-                self.optimizer_s.step()
-            s_pred , _  = self.sens_model(adj_aug, x_aug)
-            senloss = self.criterion_sens(s_pred[idx_sens],sens[idx_sens].unsqueeze(1).float())
-
-            ## update aug model
-            logits, labels = self.info_nce_loss_2views(torch.cat((h, h_prime), dim = 0))
-            contrastive_loss = self.criterion_cont(logits, labels)
-
-            ## update encoder
-            edge_loss = norm_w * F.binary_cross_entropy_with_logits(adj_logits, adj_orig.cuda())
-
-            feat_loss =  self.criterion_recons(x_aug, x)
-            recons_loss =  edge_loss + self.lam * feat_loss
-            loss = self.beta * contrastive_loss + self.gamma * recons_loss - self.alpha * senloss
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
-
-            print('Epoch: {:04d}'.format(epoch_counter+1),
-            'sens loss: {:.4f}'.format(senloss.item()),
-            'contrastive loss: {:.4f}'.format(contrastive_loss.item()),
-            'edge reconstruction loss: {:.4f}'.format(edge_loss.item()),
-            'feature reconstruction loss: {:.4f}'.format(feat_loss.item()),
-            )
-
-        self.save_path = "./checkpoint/graphair_{}_alpha{}_beta{}_gamma{}_lambda{}".format(self.dataset, self.alpha, self.beta, self.gamma, self.lam)
-        torch.save(self.state_dict(),self.save_path)
+                file.write(
+                    f'Epoch: {epoch_counter + 1:04d} sens loss: {senloss.item():.4f} contrastive loss: {contrastive_loss.item():.4f} edge reconstruction loss: {edge_loss.item():.4f} feature reconstruction loss: {feat_loss.item():.4f}\n')
+        self.save_path = "./checkpoint/graphair_{}_alpha{}_beta{}_gamma{}_lambda{}".format(self.dataset, self.alpha,
+                                                                                           self.beta, self.gamma,
+                                                                                           self.lam)
+        torch.save(self.state_dict(), self.save_path)
 
     def test(self, adj, features, labels, epochs, idx_train, idx_val, idx_test, sens):
         h = self.forward(adj, features)
@@ -223,7 +221,7 @@ class graphair(nn.Module):
 
             for i in range(5):
                 file.write(
-                    f"Experiment [{i}] \n")
+                    f"Experiment [{i + 1}] \n")
                 torch.manual_seed(i * 10)
                 np.random.seed(i * 10)
 
