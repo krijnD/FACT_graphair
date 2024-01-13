e2298d439e4166b1a676f43780cf0aaaffe53c84
Bella Gardner <isabella.v.gardner@gmail.com>
2024-01-13 11:23:00 +0100

    reverting run

---
+++ examples/fairgraph/Graphair/nbajob.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gpus=1
+#SBATCH --job-name=nbatryout
+#SBATCH --time=08:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --output=slurm_output_%A.out
+
+module purge
+module load 2022
+module load Anaconda3/2022.05
+
+# Activate your environment
+source activate fact_env
+
+srun python -u run_graphair_nba.py
