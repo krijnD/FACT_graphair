e13bfdffe22aedcbf31aa6c7e4c9019bbb5bb9ec
krijnD <krijnfelipedignum@gmail.com>
2024-01-19 21:57:25 +0100

    Update run_graphair_pokec.py

--- examples/fairgraph/Graphair/run_graphair_pokec.py
+++ examples/fairgraph/Graphair/run_graphair_pokec.py
@@ -1,14 +1,12 @@
-from dig.fairgraph.method.run import run
+from dig.fairgraph.method import run
 from dig.fairgraph.dataset import POKEC, NBA
 import torch
-import inspect
 
 # Load the dataset and split
-pokec = POKEC(dataset_sample='pokec_z') # you may also choose 'pokec_n'
+pokec = POKEC(dataset_sample='pockec_z') # you may also choose 'pockec_n'
 
 # Train and evaluate
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 run_fairgraph = run()
-print(inspect.signature(run_fairgraph.run))
 run_fairgraph.run(device,dataset=pokec,model='Graphair',epochs=2_000,batch_size=100,
             lr=1e-4,weight_decay=1e-5)
